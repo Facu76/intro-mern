837b353bbc6aea46046d4279263a14a851570b72
Facu76 <callefacundo@gmail.com>
2021-09-08 18:03:42 -0300

    actualizacion

--- intro-mern/intro-mern-backend/config.js
+++ intro-mern/intro-mern-backend/config.js
@@ -1,11 +1,12 @@
 const config = {
     appConfig:{
+        host: process.env.APP_HOST,
         port: process.env.APP_PORT
     },
     dbConfig: {
         port: process.env.DB_PORT,
         host: process.env.DB_HOST,
-        dbaName: process.env.DB_NAME
+        dbName: process.env.DB_NAME
     }
 }
 
